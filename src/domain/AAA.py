ec790f88929c5df657e3ac61af8cc985717ee46f
n-bolanos <nianbolanos@gmail.com>
2026-06-16 14:43:03 -0500

    Bases del proyecto

---
+++ src/domain/AAA.py
@@ -0,0 +1,14 @@
+class AAA:
+
+    def encrypt(self, password:str, file:bytes):
+        pass
+
+    def decrypt(self):
+        pass
+
+    def derive_pwd(self):
+        pass
+
+    def process(self):
+        pass
+print("papaya")
